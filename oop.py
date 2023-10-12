7db3e0f22c7a0ff597dd570f752fd3642376b941
Kirill Treschev <vip.wdwww@gmail.com>
2023-10-13 02:52:41 +0300

    add Assert to price and quantity

--- oop.py
+++ oop.py
@@ -1,21 +1,24 @@
 class Item:
-    def __init__(self, name, price, quantity):
+    def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
+        assert price >= 0, f"Цена {price} не должна быть меньше нуля!"
+        assert quantity >= 0, f"Количество {quantity} не должна быть меньше нуля!"
+
+
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
-        print(f"An instance created: {name}")
-    def calculate_total_price(self, x, y):
-        return x * y
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
 item1 = Item("Phone", 100, 5)
 
 item2 = Item("Laptop", 1000, 3)
 
+item2.has_numpad = False
 
-print(item1.name)
-print(item2.name)
-print(item2.price)
-print(item2.price)
-print(item2.quantity)
-print(item2.quantity)
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
